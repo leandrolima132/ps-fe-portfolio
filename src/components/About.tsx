388e03b3e9cf106b355b801a778ac3ac8fb9a0d5
Leandro Ferreira <leandro.ferreira@fortbrasil.com>
2023-05-29 18:35:14 -0300

    feat: component works

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,35 @@
+'use client'
+import CountUp from 'react-countup'
+import { useInView } from 'react-intersection-observer'
+
 export const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  })
   return (
-    <div id="about" className="section">
-      About
-    </div>
+    <section id="about" className="section" ref={ref}>
+      <div className="container mx-auto">
+        <div>
+          <div className="h-[640px] flex-1 bg-[url(/background.svg)] bg-top bg-no-repeat mix-blend-lighten ">
+            img
+          </div>
+          <div className="flex-1">
+            <h2 className="text-purple-500">Sobre me</h2>
+            <h3>blablalba</h3>
+            <p className="mb-6">askdlkaçslkdça</p>
+            <div className="flex">
+              <div>
+                <div className="text-gradient mb-2 text-[40px]">
+                  {inView ? <CountUp start={0} end={3} duration={3} /> : null}
+                </div>
+                <div className="text-sm tracking-[2px]">
+                  years of <br /> Experience
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,7 +1,92 @@
 export const Works = () => {
   return (
-    <div id="works" className="section">
-      Works
-    </div>
+    <section id="works" className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-x-10 lg:flex-row">
+          <div
+            data-aos="fade-right"
+            className="mb-10 flex flex-1 flex-col gap-y-12 lg:mb-0"
+          >
+            {/* text */}
+            <div className="h-[325px] w-full">
+              <h2 className="font-alt leading-tight text-purple-500">
+                My latest <br />
+                Work
+              </h2>
+              <p className="mb-16 max-w-sm ">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam voluptatum, quibusdam, quia, quos voluptates
+                voluptatibus voluptatem quod doloribus quas quidem repellendus?
+                Quisquam voluptatum, quibusdam, quia, quos voluptates
+                voluptatibus voluptatem quod doloribus quas quidem repellendus?
+                Quisquam voluptatum, quibusdam, quia, quos voluptates
+              </p>
+              <button className="bg-gradient rounded-full bg-black px-8 py-3 font-alt font-bold text-white">
+                View all project
+              </button>
+            </div>
+            {/* image */}
+            <div className="group relative overflow-hidden rounded-xl border-2 border-white/50">
+              {/* overlay  */}
+              <div className="absolute z-40 h-full w-full transition-all duration-300 group-hover:bg-black/70"></div>
+              {/* img */}
+              <img
+                className="transition-all duration-500 group-hover:scale-125"
+                src={'./portfolio-img1.png'}
+                alt=""
+              />
+              {/* prettier */}
+              <div className="absolute -bottom-full left-12 z-50 transition-all duration-500 group-hover:bottom-24">
+                <span className="text-gradient">Develop</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 z-50 transition-all duration-700 group-hover:bottom-14">
+                <span className="text-3xl text-white">Project Name</span>
+              </div>
+            </div>
+          </div>
+          <div data-aos="fade-left" className="flex flex-1 flex-col gap-y-10 ">
+            {/* image 1 */}
+            <div className="group relative overflow-hidden rounded-xl border-2 border-white/50">
+              {/* overlay  */}
+              <div className="absolute z-40 h-full w-full transition-all duration-300 group-hover:bg-black/70"></div>
+              {/* img */}
+              <img
+                className="transition-all duration-500 group-hover:scale-125"
+                src={'./portfolio-img1.png'}
+                alt=""
+              />
+              {/* prettier */}
+              <div className="absolute -bottom-full left-12 z-50 transition-all duration-500 group-hover:bottom-24">
+                <span className="text-gradient">Develop</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 z-50 transition-all duration-700 group-hover:bottom-14">
+                <span className="text-3xl text-white">Project Name</span>
+              </div>
+            </div>
+            {/* image 2 */}
+            <div className="group relative overflow-hidden rounded-xl border-2 border-white/50">
+              {/* overlay  */}
+              <div className="absolute z-40 h-full w-full transition-all duration-300 group-hover:bg-black/70"></div>
+              {/* img */}
+              <img
+                className="transition-all duration-500 group-hover:scale-125"
+                src={'./portfolio-img1.png'}
+                alt=""
+              />
+              {/* prettier */}
+              <div className="absolute -bottom-full left-12 z-50 transition-all duration-500 group-hover:bottom-24">
+                <span className="text-gradient">Develop</span>
+              </div>
+              {/* title */}
+              <div className="absolute -bottom-full left-12 z-50 transition-all duration-700 group-hover:bottom-14">
+                <span className="text-3xl text-white">Project Name</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
