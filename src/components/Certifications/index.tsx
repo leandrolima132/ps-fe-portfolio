840c6692298bae89cfe55e5c71bcf1f496cbd498
Leandro Ferreira <leandrolima_ferreira@hotmail.com>
2023-06-04 15:51:15 -0300

    feat: Certifications

---
+++ src/components/Certifications/index.tsx
@@ -0,0 +1,12 @@
+export const Certifications = () => {
+  return (
+    <section className="section" id="certifications">
+      <div className="container text-gray-600">
+        <div className="flex flex-col items-start">
+          <h1 className="font-poppins text-2xl font-bold">Certifications</h1>
+          {/* Cartão */}
+        </div>
+      </div>
+    </section>
+  )
+}
